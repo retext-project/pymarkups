15a03f577078a004367604dc3d959dbd930ee330
Dmitry Shachnev <mitya57@gmail.com>
2012-08-27 15:53:32 +0400

    MarkdownMarkup: add MathJax support

--- markups/markdown.py
+++ markups/markdown.py
@@ -2,6 +2,9 @@
 # License: BSD
 # Copyright: (C) Dmitry Shachnev, 2012
 
+# MathJax pattern code is based on work by Rob Mayoff
+# https://github.com/mayoff/python-markdown-mathjax
+
 import sys
 from markups.core import *
 from markups.abstract import AbstractMarkup
@@ -36,6 +39,15 @@ class MarkdownMarkup(AbstractMarkup):
 			extensions_file.close()
 			return extensions
 	
+	def _get_mathjax_pattern(self, markdown):
+		def handleMatch(m):
+			node = markdown.util.etree.Element('mathjax')
+			node.text = markdown.util.AtomicString(m.group(2) + m.group(3) + m.group(2))
+			return node
+		pattern = markdown.inlinepatterns.Pattern(r'(?<!\\)(\$\$?)(.+?)\2')
+		pattern.handleMatch = handleMatch
+		return pattern
+	
 	def __init__(self, filename=None):
 		import markdown
 		self.extensions = self._load_extensions_list_from_file(
@@ -58,6 +70,8 @@ class MarkdownMarkup(AbstractMarkup):
 				sys.stderr.write(e)
 				sys.stderr.write('\n')
 				self.md = markdown.Markdown(output_format='html4')
+		self.md.inlinePatterns.add('mathjax', self._get_mathjax_pattern(markdown),
+		'<escape')
 	
 	def get_document_title(self, text):
 		try:
@@ -69,6 +83,12 @@ class MarkdownMarkup(AbstractMarkup):
 		if 'codehilite' in self.extensions + self.local_extensions:
 			return get_pygments_stylesheet('.codehilite')
 		return ''
+	
+	def get_javascript(self, text='', webenv=False):
+		return ('<script type="text/javascript" src="' + get_mathjax_url(webenv)
+		+ '?config=TeX-AMS-MML_HTMLorMML"></script>\n<script type="text/javascript">\n' +
+		'MathJax.Hub.Config({ "tex2jax": { inlineMath: [[ \'$\', \'$\' ]] } });\n'
+		+ '</script>\n')
 	
 	def get_document_body(self, text):
 		self.md.reset()
