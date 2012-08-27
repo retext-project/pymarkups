1391d3ab8de9ad5aeb248139341f474efa004638
Dmitry Shachnev <mitya57@gmail.com>
2012-08-27 21:39:50 +0400

    * Add tags support for get_javascript * MarkdownMarkup: only add MathJax code if mathjax tag present

--- markups/abstract.py
+++ markups/abstract.py
@@ -21,7 +21,7 @@ class AbstractMarkup(object):
 	def get_stylesheet(self, text=''):
 		return ''
 	
-	def get_javascript(self, text='', webenv=False):
+	def get_javascript(self, text='', webenv=False, tags=[]):
 		return ''
 	
 	def get_whole_html(self, text, custom_headers='', include_stylesheet=True,
@@ -32,11 +32,13 @@ class AbstractMarkup(object):
 		if not title:
 			title = fallback_title
 		title_string = ('<title>' + title + '</title>\n') if title else ''
+		body = self.get_document_body(text)
+		tags = ['mathjax'] if ('<math' in body) else []
 		return (
 		'<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">\n'
 		'<html>\n<head>\n'
 		'<meta http-equiv="content-type" content="text/html; charset=utf-8">\n'
 		+ custom_headers + title_string + stylesheet +
-		self.get_javascript(text, webenv) + '</head>\n<body>\n' +
-		self.get_document_body(text) + '</body>\n</html>\n'
+		self.get_javascript(text, webenv, tags) + '</head>\n<body>\n'
+		+ body + '</body>\n</html>\n'
 		)
