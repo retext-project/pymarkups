b175dcea534acd55e6cb5649c2833100407d225b
Dmitry Shachnev <mitya57@gmail.com>
2012-09-04 19:10:44 +0400

    First test: testing web module

---
+++ tests/test_web.py
@@ -0,0 +1,33 @@
+import markups.web
+import os
+import difflib
+import sys
+
+def test_web():
+	if os.path.exists('tests/data'):
+		working_dir='tests/data'
+	elif os.path.exists('data'):
+		working_dir='data'
+	else:
+		sys.exit('Could not find data directory!')
+	app_data = ('test', '1.0', 'http://example.com/')
+	lib = markups.web.WebLibrary(working_dir, app_data)
+	lib.update('page.rst')
+	expected_file = open(working_dir+'/page.html')
+	expected_output = [line for line in expected_file]
+	expected_file.close()
+	html_file = open(working_dir+'/html/page.html')
+	html_output = [line for line in html_file]
+	html_file.close()
+	diff = difflib.unified_diff(expected_output, html_output,
+		fromfile=working_dir+'/page.html',
+		tofile=working_dir+'/html/page.html')
+	diff = [line for line in diff]
+	os.remove(working_dir+'/html/page.html')
+	os.rmdir(working_dir+'/html')
+	if diff:
+		sys.stderr.write(str.join('', diff))
+		sys.exit('Web test failed: diff is not empty')
+
+if __name__ == '__main__':
+	test_web()
