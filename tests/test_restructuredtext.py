b5c6156c07540fdb25d53a16883a558abedee7d8
Dmitry Shachnev <mitya57@gmail.com>
2012-11-17 18:57:23 +0400

    tests: use unittest module

--- tests/test_restructuredtext.py
+++ tests/test_restructuredtext.py
@@ -2,7 +2,7 @@
 # License: BSD
 # Copyright: (C) Dmitry Shachnev, 2012
 
-import sys
+import unittest
 from markups import ReStructuredTextMarkup
 
 basic_text = \
@@ -11,35 +11,27 @@ basic_text = \
 
 This is an example **reStructuredText** document.'''
 
-def fail_test(message):
-	sys.exit('reStructuredText test failed: '+message)
-
-def test_basic():
-	markup = ReStructuredTextMarkup()
-	text = markup.get_document_body(basic_text)
-	title = markup.get_document_title(basic_text)
-	markup.enable_cache = True
-	text_from_cache = markup.get_document_body(basic_text)
-	title_from_cache = markup.get_document_title(basic_text)
-	text_expected = \
-	'<p>This is an example <strong>reStructuredText</strong> document.</p>\n'
-	title_expected = 'Hello, world!'
-	if text != text_expected or text_from_cache != text_expected:
-		fail_text('output does not match expexted')
-	if title != title_expected or title_from_cache != title_expected:
-		fail_text('title does not match expexted')
-
-def test_mathjax_loading():
-	markup = ReStructuredTextMarkup()
-	if markup.get_javascript('Hello, world!'):
-		fail_test('get_javascript() returned non-empty string')
-	js = markup.get_javascript('Hello, :math:`2+2`!')
-	if not '<script' in js:
-		fail_test('mathjax script not included')
-	body = markup.get_document_body('Hello, :math:`2+2`!')
-	if not ('<span class="math">' in body and r'\(2+2\)</span>' in body):
-		fail_test('math not working')
+class ReStructuredTextTest(unittest.TestCase):
+	def test_basic(self):
+		markup = ReStructuredTextMarkup()
+		text = markup.get_document_body(basic_text)
+		title = markup.get_document_title(basic_text)
+		markup.enable_cache = True
+		text_from_cache = markup.get_document_body(basic_text)
+		title_from_cache = markup.get_document_title(basic_text)
+		text_expected = \
+		'<p>This is an example <strong>reStructuredText</strong> document.</p>\n'
+		title_expected = 'Hello, world!'
+		self.assertEqual(text_expected, text)
+		self.assertEqual(text_expected, text_from_cache)
+	
+	def test_mathjax_loading(self):
+		markup = ReStructuredTextMarkup()
+		self.assertEqual('', markup.get_javascript('Hello, world!'))
+		js = markup.get_javascript('Hello, :math:`2+2`!')
+		self.assertTrue('<script' in js)
+		body = markup.get_document_body('Hello, :math:`2+2`!')
+		self.assertTrue('<span class="math">' in body and r'\(2+2\)</span>' in body)
 
 if __name__ == '__main__':
-	test_basic()
-	test_mathjax_loading()
+	unittest.main()
--- tests/test_web.py
+++ tests/test_web.py
@@ -4,37 +4,36 @@
 
 import markups.web
 import os
-import difflib
-import sys
+import unittest
 
-def test_web():
-	if os.path.exists('tests/data'):
-		working_dir='tests/data'
-	elif os.path.exists('data'):
-		working_dir='data'
-	else:
-		sys.exit('Could not find data directory!')
-	app_data = ('test', '1.0', 'http://example.com/')
-	lib = markups.web.WebLibrary(working_dir, app_data)
-	lib.update('page.rst')
-	expected_file = open(working_dir+'/page.html')
-	expected_output = [line for line in expected_file]
-	expected_file.close()
-	html_file = open(working_dir+'/html/page.html')
-	html_output = [line for line in html_file]
-	html_file.close()
-	diff = difflib.unified_diff(expected_output, html_output,
-		fromfile=working_dir+'/page.html',
-		tofile=working_dir+'/html/page.html')
-	diff = [line for line in diff]
-	os.remove(working_dir+'/html/page.html')
-	lib.update_all()
-	assert os.path.exists(working_dir+'/html/page.html')
-	os.remove(working_dir+'/html/page.html')
-	os.rmdir(working_dir+'/html')
-	if diff:
-		sys.stderr.write(str.join('', diff))
-		sys.exit('Web test failed: diff is not empty')
+class WebTest(unittest.TestCase):
+	def setUp(self):
+		if os.path.exists('tests/data'):
+			self.working_dir='tests/data'
+		elif os.path.exists('data'):
+			self.working_dir='data'
+		else:
+			raise RuntimeError('Could not find data directory!')
+		self.get_file = lambda fn: os.path.join(self.working_dir, fn)
+	
+	def test_web(self):	
+		app_data = ('test', '1.0', 'http://example.com/')
+		lib = markups.web.WebLibrary(self.working_dir, app_data)
+		lib.update('page.rst')
+		expected_file = open(self.get_file('page.html'))
+		expected_output = expected_file.read()
+		expected_file.close()
+		html_file = open(self.get_file('html/page.html'))
+		html_output = html_file.read()
+		html_file.close()
+		self.assertEqual(expected_output, html_output)
+		os.remove(self.get_file('html/page.html'))
+		lib.update_all()
+		self.assertTrue(os.path.exists(self.get_file('html/page.html')))
+	
+	def tearDown(self):
+		os.remove(self.get_file('html/page.html'))
+		os.rmdir(self.get_file('html'))
 
 if __name__ == '__main__':
-	test_web()
+	unittest.main()
