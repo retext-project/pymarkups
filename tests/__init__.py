d56d91c9adc2fe7d8e53f2f01293c3d6def937a2
Dmitry Shachnev <mitya57@gmail.com>
2012-11-18 14:03:00 +0400

    tests/__init__.py: don't import individual tests if that file is imported from setup.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,15 +1,17 @@
 import sys
+import unittest
+
+if sys.version_info[0] < 3 and 'setup.py' not in sys.argv[0]:
+	from test_public_api import APITest
+	from test_markdown import MarkdownTest
+	from test_restructuredtext import ReStructuredTextTest
+	from test_web import WebTest
 
 def main():
 	if sys.version_info[0] >= 3:
 		from unittest.main import main
 		main(module=None)
 	else:
-		import unittest
-		from .test_public_api import APITest
-		from .test_markdown import MarkdownTest
-		from .test_restructuredtext import ReStructuredTextTest
-		from .test_web import WebTest
 		unittest.main()
 
 if __name__ == '__main__':
