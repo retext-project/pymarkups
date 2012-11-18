e5670f5fe04b907bbd66cf789726a032a6d56e73
Dmitry Shachnev <mitya57@gmail.com>
2012-11-18 12:56:41 +0400

    tests/__init__.py: rewrite to make it compatible with Python 2

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,11 @@
+import unittest
+from test_public_api import APITest
+from test_markdown import MarkdownTest
+from test_restructuredtext import ReStructuredTextTest
+from test_web import WebTest
+
 def main():
-	from unittest.main import main
-	main(module=None)
+	unittest.main()
 
 if __name__ == '__main__':
 	main()
