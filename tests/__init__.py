97a9bb7a591810b9538389307c46c36b9d8919bb
Dmitry Shachnev <mitya57@gmail.com>
2013-04-05 18:07:50 +0400

    Drop no longer used code from tests/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,6 @@
 import sys
 import unittest
 
-if sys.version_info[0] < 3 and 'setup.py' not in sys.argv[0]:
-	from test_public_api import APITest
-	from test_markdown import MarkdownTest
-	from test_restructuredtext import ReStructuredTextTest
-	from test_web import WebTest
-
 def main():
 	if sys.version_info[0] >= 3:
 		from unittest.main import main
