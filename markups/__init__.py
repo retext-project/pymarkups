1afb3f3c6cba3534d3ce90c23c076904d686ed65
Dmitry Shachnev <mitya57@gmail.com>
2013-07-25 14:22:45 +0400

    Releasing 0.3

--- markups/__init__.py
+++ markups/__init__.py
@@ -8,7 +8,7 @@ from markups.common import CONFIGURATION_DIR
 from markups.markdown import MarkdownMarkup
 from markups.restructuredtext import ReStructuredTextMarkup
 
-__version__ = '0.2.4'
+__version__ = '0.3'
 
 builtin_markups = [MarkdownMarkup, ReStructuredTextMarkup]
 
