680ce5499b07c4201e56bf7eefbc40c5550100bf
Dmitry Shachnev <mitya57@gmail.com>
2013-11-25 23:06:34 +0400

    Refactor test_web.py

--- tests/test_web.py
+++ tests/test_web.py
@@ -5,35 +5,42 @@
 import markups.web
 import os
 import unittest
+from markups.restructuredtext import ReStructuredTextMarkup
 
 class WebTest(unittest.TestCase):
-	def setUp(self):
+	@classmethod
+	def setUpClass(cls):
 		if os.path.exists('tests/data'):
-			self.working_dir='tests/data'
+			working_dir='tests/data'
 		elif os.path.exists('data'):
-			self.working_dir='data'
+			working_dir='data'
 		else:
 			raise RuntimeError('Could not find data directory!')
-		self.get_file = lambda fn: os.path.join(self.working_dir, fn)
-
-	def test_web(self):
+		cls.get_file = staticmethod(lambda fn: os.path.join(working_dir, fn))
 		app_data = ('test', '1.0', 'http://example.com/')
-		lib = markups.web.WebLibrary(self.working_dir, app_data)
-		lib.update('page.rst')
-		expected_file = open(self.get_file('page.html'))
-		expected_output = expected_file.read()
-		expected_file.close()
-		html_file = open(self.get_file('html/page.html'))
-		html_output = html_file.read()
-		html_file.close()
+		cls.lib = markups.web.WebLibrary(working_dir, app_data)
+
+	@unittest.skipUnless(ReStructuredTextMarkup.available(), 'Docutils not available')
+	def test_web(self):
+		self.lib.update('page.rst')
+		with open(self.get_file('page.html')) as expected_file:
+			expected_output = expected_file.read()
+		with open(self.get_file('html/page.html')) as html_file:
+			html_output = html_file.read()
 		self.assertEqual(expected_output, html_output)
 		os.remove(self.get_file('html/page.html'))
-		lib.update_all()
+		self.lib.update_all()
 		self.assertTrue(os.path.exists(self.get_file('html/page.html')))
 
-	def tearDown(self):
-		os.remove(self.get_file('html/page.html'))
-		os.rmdir(self.get_file('html'))
+	@classmethod
+	def tearDownClass(cls):
+		if os.path.exists(cls.get_file('html/page.html')):
+			os.remove(cls.get_file('html/page.html'))
+		if os.path.exists(cls.get_file('html')):
+			os.rmdir(cls.get_file('html'))
+
+if not hasattr(WebTest, 'assertRaisesRegex'):
+	WebTest.assertRaisesRegex = WebTest.assertRaisesRegexp
 
 if __name__ == '__main__':
 	unittest.main()
