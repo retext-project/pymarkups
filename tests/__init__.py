01e62119750d0737e396358dbf45727dcbb5732f
Dmitry Shachnev <mitya57@gmail.com>
2014-08-08 20:46:55 +0400

    Drop Python 2 support in tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,4 @@
-import sys
-import unittest
-
-def main():
-	if sys.version_info[0] >= 3:
-		from unittest.main import main
-		main(module=None)
-	else:
-		unittest.main()
+from unittest.main import main
 
 if __name__ == '__main__':
-	main()
+	main(module=None, verbosity=2)
