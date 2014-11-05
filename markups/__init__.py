cc640b6a09ce6f43a25eda3bb6d9db586a9f7157
Dmitry Shachnev <mitya57@gmail.com>
2014-11-05 20:48:42 +0300

    Releasing 0.5.2

--- markups/__init__.py
+++ markups/__init__.py
@@ -9,7 +9,7 @@ from markups.markdown import MarkdownMarkup
 from markups.restructuredtext import ReStructuredTextMarkup
 from markups.textile import TextileMarkup
 
-__version_tuple__ = (0, 5, 1)
+__version_tuple__ = (0, 5, 2)
 __version__ = '.'.join(map(str, __version_tuple__))
 
 builtin_markups = [MarkdownMarkup, ReStructuredTextMarkup, TextileMarkup]
