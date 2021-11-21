cf47ceaa920e377cb7a7220517134a6f767dd1c0
Dmitry Shachnev <mitya57@gmail.com>
2021-11-21 22:02:20 +0300

    Releasing 3.1.3

--- markups/__init__.py
+++ markups/__init__.py
@@ -8,7 +8,7 @@ from markups.markdown import MarkdownMarkup
 from markups.restructuredtext import ReStructuredTextMarkup
 from markups.textile import TextileMarkup
 
-__version_tuple__ = (3, 1, 2)
+__version_tuple__ = (3, 1, 3)
 __version__ = '.'.join(map(str, __version_tuple__))
 
 builtin_markups = [MarkdownMarkup, ReStructuredTextMarkup, TextileMarkup]
